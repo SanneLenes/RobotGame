0f5800033e1adc757ec5dfe71f4bd48f6919fab0
SanneLenes <Sannelenes@gmail.com>
2025-11-11 14:53:36 +0000

    Nieuwe tekst

--- js/levels.js
+++ js/levels.js
@@ -6,7 +6,7 @@ window.LEVELS = [
     goal:  { r: 0, c: 2 },
     obstacles: [{ r: 1, c: 1 }],
     maxSteps: 4,
-    story: { title: "Level 1: Opstarten", text: "Ik ben Lina, developer. Jij bedenkt het plan, mijn robot voert het uit." },
+    story: { title: "Level 1", text: "Ik ben Lina en ik heb mijn eigen robot gemaakt" },
   },
   {
     gridSize: 3,
@@ -14,7 +14,7 @@ window.LEVELS = [
     goal:  { r: 0, c: 2 },
     obstacles: [{ r: 1, c: 0 }, { r: 1, c: 1 }],
     maxSteps: 5,
-    story: { title: "Level 2: Debuggen", text: "Obstakels horen erbij. We passen het plan aan en testen opnieuw." },
+    story: { title: "Level 2", text: "Obstakels horen erbij. We passen het plan aan en testen opnieuw." },
   },
   {
     gridSize: 4,
@@ -22,12 +22,12 @@ window.LEVELS = [
     goal:  { r: 0, c: 3 },
     obstacles: [{ r: 2, c: 1 }, { r: 1, c: 2 }, { r: 2, c: 2 }],
     maxSteps: 6,
-    story: { title: "Level 3: Flow", text: "Je denkt nu écht in stappen. Dit is programmeren zonder code." },
+    story: { title: "Level 3", text: "Je denkt goed in stappen" },
   },
 ];
 
 // Intro-zinnen van Lina (wisselen automatisch)
 window.INTRO_LINES = [
-  "Hé, jij daar. Jij lijkt me slim genoeg om dit te fixen! Mijn robot werkt… bijna. Maar mijn code zit vast.",
+  "Hé, jij daar. Jij lijkt me slim genoeg om dit te fixen!",
 
 ];
